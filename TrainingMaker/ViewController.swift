69978d52894c589a1c2c7965cd0c1c71efaf42d8
adeveloper2015 <hudiakov@gmail.com>
2017-07-21 16:45:43 +0300

    Add Xcode project

---
+++ TrainingMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingMaker
+//
+//  Created by Kirill Khudyakov on 21.07.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
